4fb1969d056647e413e8fb9edd2bca940ee2f7f5
unknown <achidera20@gmail.com>
2021-12-19 17:32:09 +0100

    ready

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,70 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { useState } from 'react';
 
-export default function Home() {
+const RegisterPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const registerUser = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+  };
+
+  const formInputs = [
+    {
+      label: 'Email',
+      name: 'email',
+      type: 'email',
+      placeholder: 'Enter your email',
+      ariaLabel: 'formEmail',
+    },
+    {
+      label: 'Phone',
+      name: 'phone',
+      type: 'tel',
+      placeholder: 'Enter your phone number',
+      ariaLabel: 'formPhone',
+    },
+    {
+      label: 'Password',
+      name: 'password',
+      type: 'password',
+      placeholder: 'Enter your password',
+      ariaLabel: 'formPassword',
+    },
+    {
+      label: 'Confirm Password',
+      name: 'confirmPassword',
+      type: 'password',
+      placeholder: 'Confirm your password',
+      ariaLabel: 'formConfirmPassword',
+    }
+  ];
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-      </Head>
+    <div className="page-wrapper">
+      <form>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        {formInputs.map((input, index) => {
+        return ( <div className="form-group" key={index}>
+                <label>{input.label}</label>
+                <input
+                    name={input.name}
+                    type={input.type}
+                    aria-label={input.ariaLabel}
+                    placeholder={input.placeholder}
+                  />
+              </div>)
+        })}
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-        </a>
-      </footer>
+              
+              <button variant="light" onClick={registerUser}>
+                {isLoading ? 'Loading...' : 'Create account'}
+              </button>
+            </form>
     </div>
   );
-}
+};
+
+export default RegisterPage;
